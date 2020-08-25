1def5eb9df80f3b20312075ce6af00a22a2234b9
BenD593 <ben.xavier.davies@gmail.com>
2020-08-25 13:16:04 +0100

    remove install files

--- install.sh
+++
@@ -1,8 +0,0 @@
-echo "SkyLab Panel is going to install some basics. It will then start install.py"
-apt-get update -y
-apt-get install python3 -y
-apt-get install python3-pip -y
-pip3 install flask
-pip3 install bcrypt
-pip3 install mysql-connector-python
-python3 install.py
