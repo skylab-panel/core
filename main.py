8fc6bcba7b9ee62f1150f8fec064e61efc763f26
BenD593 <ben.xavier.davies@gmail.com>
2020-08-31 19:28:17 +0200

    added user managment pages

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ import mariadb
 import bcrypt
 import logging
 import sys
-from collections import defaultdict
-
+from flask_wtf import FlaskForm
+from wtforms import *
+from wtforms.validators import DataRequired
+import functools
 ######################
 ### Main Variables ###
 ######################
@@ -18,6 +20,7 @@ lines = main_config.readlines()
 
 
 logging.basicConfig(filename='example.log',level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%m/%d %H:%M:%S')
+
 
 ###########################
 ### Database Conection ####
@@ -97,16 +100,176 @@ def logout():
 @app.route('/client/user-management/users')
 def user_managment():
     cur.execute("USE skylabpanel")
-    cur.execute("SELECT id, username, domains, package, account_type FROM tbl_users WHERE account_type != ?", ('admin', ))
+    cur.execute("SELECT user_id, username, firstname, lastname, email, account_type FROM tbl_users WHERE account_type != ?", ('admin', ))
     myresult = cur.fetchall()
     num_records = len(myresult)
     all_customers = []
     for row in myresult:
-        customer = [row[0], row[1], row[2], row[3], row[4]]
+        customer = [row[0], row[1], row[2], row[3], row[4], row[5]]
         all_customers.append(customer)
         customer = []
-    return render_template('/client/user-management/users.html', results=all_customers)
+    return render_template('/client/user-management/users.html', results=all_customers, num_records=num_records)
 
+@app.route('/client/user-management/add-user')
+def user_managment_add_user():
+    return render_template('/client/user-management/user-add.html')
+@app.route('/client/user-management/edit-user')
+def user_managment_edit_user():
+    return render_template('/client/user-management/user-edit.html')
+
+@app.route('/client/user-management/remove-user', methods=('GET', 'POST'))
+def user_managment_remove_user():
+    class myform(FlaskForm):
+        cur.execute("USE skylabpanel")
+        cur.execute("SELECT username FROM tbl_users WHERE account_type != ?", ('admin', ))
+        myresult = cur.fetchall()
+        myresult = functools.reduce(lambda x,y: x+y, myresult)
+        users = SelectField('User Name', choices=myresult, validators=[DataRequired()], default='cpp')
+        submit = SubmitField('Submit')
+    form = myform()
+    if form.is_submitted():
+        cur.execute("DELETE FROM tbl_users WHERE username = ?", (form.users.data, ))
+        flash('User: ' + form.users.data + ' Removed', 'info')
+        return redirect(url_for('user_managment'))
+    return render_template('/client/user-management/remove-user.html', form=form) 
+
+######################
+### package Managment ###
+#######################
+@app.route('/client/package-management/packages')
+def package_managment():
+    cur.execute("USE skylabpanel")
+    cur.execute("SELECT * FROM tbl_packages")
+    myresult = cur.fetchall()
+    num_records = len(myresult)
+    all_packages = []
+    for row in myresult:
+        package = [row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8]]
+        all_packages.append(package)
+        package = []
+    return render_template('/client/package-management/packages.html', results=all_packages, num_records=num_records)
+
+@app.route('/client/package-management/add-package',  methods=('GET', 'POST'))
+def package_managment_add_package():
+    class add_package_form(FlaskForm):
+        package_name = StringField('Package Name', validators=[DataRequired()])
+        package_cost = DecimalField('Package Price', validators=[DataRequired()]) 
+        max_domains = IntegerField('Max Number of Domains')
+        max_sub_domains = IntegerField('Max Number of Sub Domains')
+        max_storage = IntegerField('Max Ammout of Storage ')
+        max_ftp_accounts = IntegerField('Max Number of FTP Accounts ')
+        max_databases = IntegerField('Max Number of Databases ')
+        max_email_accounts = IntegerField('Max Number of Email Accounts')
+        submit = SubmitField('Submit')
+
+    form = add_package_form()
+    if form.is_submitted():
+        cur.execute("USE skylabpanel")
+        cur.execute("""INSERT INTO tbl_packages(
+            package_name, 
+            package_cost, 
+            max_domains, 
+            max_sub_domains, 
+            max_storage, 
+            max_ftp_accounts, 
+            max_databases,
+            max_email_accounts)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)""",
+            (form.package_name.data, 
+            form.package_cost.data, 
+            form.max_domains.data, 
+            form.max_sub_domains.data, 
+            form.max_storage.data, 
+            form.max_ftp_accounts.data, 
+            form.max_databases.data, 
+            form.max_email_accounts.data))
+        flash('Package: ' + form.package_name.data + ' Added', 'info')
+        return redirect(url_for('package_managment'))
+    return render_template('/client/package-management/add-package.html', form=form)
+@app.route('/client/package-management/edit-package', methods=('GET', 'POST'))
+def package_managment_edit_package():
+    class edit_package_form_p1(FlaskForm):
+        cur.execute("USE skylabpanel")
+        cur.execute("SELECT package_name FROM tbl_packages")
+        myresult = cur.fetchall()
+        myresult = functools.reduce(lambda x,y: x+y, myresult)
+        package_name = SelectField(u'Select Package', choices=myresult, validators=[DataRequired()])
+        submit = SubmitField('Submit')
+    form = edit_package_form_p1()
+    if form.is_submitted():
+        package_managment_edit_package.package_name = form.package_name.data
+        return redirect(url_for('package_managment_edit_package_1'))
+    return render_template('/client/package-management/edit-package.html', form=form)
+
+@app.route('/client/package-management/edit-package-1', methods=('GET', 'POST'))
+def package_managment_edit_package_1():
+    cur.execute("USE skylabpanel")
+    cur.execute("SELECT package_cost, max_domains, max_sub_domains, max_storage, max_ftp_accounts, max_databases, max_email_accounts FROM tbl_packages WHERE package_name = ?", ( package_managment_edit_package.package_name , ))
+    myresult = cur.fetchall()
+    myresult = functools.reduce(lambda x,y: x+y, myresult)    
+    class edit_package_form_p2(FlaskForm):
+            package_cost = DecimalField('Package Price', validators=[DataRequired()], default=myresult[0]) 
+            max_domains = IntegerField('Max Number of Domains', default=myresult[1])
+            max_sub_domains = IntegerField('Max Number of Sub Domains', default=myresult[2])
+            max_storage = IntegerField('Max Ammout of Storage ', default=myresult[3])
+            max_ftp_accounts = IntegerField('Max Number of FTP Accounts ', default=myresult[4])
+            max_databases = IntegerField('Max Number of Databases ', default=myresult[5])
+            max_email_accounts = IntegerField('Max Number of Email Accounts', default=myresult[6])
+            submit = SubmitField('Submit')
+    form = edit_package_form_p2()
+    if form.is_submitted():
+        cur.execute("UPDATE tbl_packages SET package_cost = ?, max_domains = ?, max_sub_domains = ?, max_storage = ?, max_ftp_accounts = ?, max_databases = ?, max_email_accounts = ? WHERE package_name = ?", (form.package_cost.data, form.max_domains.data, form.max_sub_domains.data, form.max_storage.data, form.max_ftp_accounts.data, form.max_databases.data, form.max_email_accounts.data, package_managment_edit_package.package_name))
+        flash('Package: ' + package_managment_edit_package.package_name + ' Updated', 'info')
+        return redirect(url_for('package_managment'))
+    return render_template('/client/package-management/edit-package-1.html', form=form)
+
+@app.route('/client/package-management/remove-package', methods=('GET', 'POST'))
+def package_managment_remove_package():
+    class remove_package_form(FlaskForm):
+        cur.execute("USE skylabpanel")
+        cur.execute("SELECT package_name FROM tbl_packages")
+        myresult = cur.fetchall()
+        myresult = functools.reduce(lambda x,y: x+y, myresult)
+        packages = SelectField(u'Select Package', choices=myresult, validators=[DataRequired()], default='cpp')
+        submit = SubmitField('Submit')
+    form = remove_package_form()
+    if form.is_submitted():
+        cur.execute("DELETE FROM tbl_packages WHERE package_name = ?", (form.packages.data, ))
+        flash('Package: ' + form.packages.data + ' Removed', 'info')
+        return redirect(url_for('package_managment'))
+    return render_template('/client/package-management/remove-package.html', form=form) 
+
+##########################
+### Database Managment ###
+##########################
+@app.route('/client/database-management/databases')
+def database_management():
+    cur.execute("USE skylabpanel")
+    cur.execute("SELECT Db, Host, User from mysql.db WHERE Db NOT IN (?, ?)", ('phpmyadmin', 'skylabpanel'))
+    myresult = cur.fetchall()
+    num_records = len(myresult)
+    all_databases = []
+    for row in myresult:
+        database = [row[0].decode("utf-8"), row[1].decode("utf-8"), row[2].decode("utf-8")]
+        all_databases.append(database)
+        database = []
+    return render_template('/client/database-management/databases.html', results=all_databases, num_records=num_records)
+
+@app.route('/client/database-management/add-database' , methods=('GET', 'POST'))
+def database_management_add_database():
+    class add_database_form(FlaskForm):
+        db_name = StringField('Database Name')
+        submit = SubmitField('Submit')
+    form = add_database_form()
+    if form.is_submitted():
+        db_name = session['username'] + "_"+ form.db_name.data
+        print(db_name)
+        cur.execute("CREATE DATABASE IF NOT EXISTS " + db_name)
+        cur.execute("USE " + db_name)
+        cur.execute("GRANT ALL PRIVILEGES ON * TO " + session['username'] +"@'localhost' WITH GRANT OPTION;")
+        flash('Database: ' + db_name + ' Added', 'info')
+        return redirect(url_for('database_management'))
+    return render_template('/client/database-management/add-database.html', form=form)
 ###########
 ### Run ###
 ###########
